3871ffd526d9586da60728921175459b73896d5a
Константин Орлов <konsor-79@mail.ru>
2023-12-30 20:42:33 +0300

    init

---
+++ src/main/java/com/github/uwak9073/pg_service/PgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.uwak9073.pg_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PgServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/uwak9073/pg_service/PgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.uwak9073.pg_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
